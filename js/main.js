b1ac292df2983917a45c824f52a3575f6379d558
Denis Rohlinsky <denis.rohlinsky@gmail.com>
2019-12-08 16:25:50 +0200

    minimize js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 function addKeyboard () {
 	let list = {
 		letter: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-		number: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
 		symbol: [",", ".", "\/"]
 	},
 	storage = "",
 	register = false,
 	display = 1,
-	input
+	input,
+	i
 	$('div.keyboard').append('<div class="number list"></div>')
 					 .append('<div class="letter list"></div>')
 					 .append('<div class="symbol list"></div>')
@@ -23,22 +23,18 @@ function addKeyboard () {
 	list.letter.map(function (item) {
 		$('div.keyboard div.letter.list').append('<div class="button letter">' + item + '</div>')
 	})
-	list.number.map(function (item) {
-		$('div.keyboard div.number.list').append('<div class="button number">' + item + '</div>')
-	})
 	list.symbol.map(function (item) {
 		$('div.keyboard div.symbol.list').append('<div class="button symbol">' + item + '</div>')
 	})
+	for (i = 0; i <= 9; i++) {
+		$('div.keyboard div.number.list').append('<div class="button number">' + ((i !== 9) ? (i + 1) : 0) + '</div>')
+	}
 	$('div.keyboard div.control button.caps.lock').click(function () {
 		register = !register
 		$('div.keyboard div.letter.list div').each(function (index) {
 			$(this).css('text-transform', (register) ? 'uppercase' : 'lowercase')
 		})
 		$(this).text((register) ? "↓" : "↑")
-		// $('div.keyboard div.letter.list').html("")
-		// list.letter[(register) ? "uppercase" : "lowercase"].map(function (item) {
-		// 	$('div.keyboard div.letter.list').append('<div class="button letter">' + item + '</div>')
-		// })
 	})
 	$('div.keyboard div.control button.alt').click(function () {
 		display = !display
@@ -52,10 +48,6 @@ function addKeyboard () {
 			$('div.keyboard div.symbol.list').show()
 			$('div.keyboard div.letter.list').hide()
 		}
-		// $('div.keyboard div.letter.list').html("")
-		// list.letter[(register) ? "uppercase" : "lowercase"].map(function (item) {
-		// 	$('div.keyboard div.letter.list').append('<div class="button letter">' + item + '</div>')
-		// })
 	})
 	$('input').on('click', function () {
 		input = $(this)
@@ -70,7 +62,6 @@ function addKeyboard () {
 	$('div.keyboard div.control button.copy').click(function () {
 		if (input) {
 			input.select();
-			// input.setSelectionRange(0, 99999); 
 			document.execCommand("copy");
 		}
 	})
@@ -79,4 +70,6 @@ function addKeyboard () {
 			storage += " "
 		}
 	})
+	list = undefined
+	i = undefined
 }
